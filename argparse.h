c0b71bb4abe13bb874383a2ee031783c4f65077f
kkarbowiak <kkarbowiak@localhost>
2018-04-25 22:25:01 +0200

    Extend Argument with ability to return held value

--- argparse.h
+++ argparse.h
@@ -35,6 +35,11 @@ namespace argparse
                 return args;
             }
 
+            std::string get_value() const
+            {
+                return m_value;
+            }
+
         private:
             std::string const m_name;
             std::string m_value;
