0808dd417e1298ca9f2e493d1d2668ed757c5837
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2019-10-06 23:58:58 +0200

    Add reference test script taking one optional and one positional argument

---
+++ test/reference/test_01op.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Test description')
+parser.add_argument('-o', help='optional arg #1')
+parser.add_argument('pos1', help='positional arg #1')
+
+args = parser.parse_args()
+
+print(args)
