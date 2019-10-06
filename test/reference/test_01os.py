aabfc44845720a5d75304dfc44b6f2ab15d2207f
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2019-10-06 23:44:14 +0200

    Add reference test script taking one optional argument with short name

--- test/reference/test_01os.py
+++ test/reference/test_01os.py
@@ -2,7 +2,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Test description')
-parser.add_argument('-o', '--opt1', help='optional arg #1')
+parser.add_argument('-a', help='optional arg #1')
 
 args = parser.parse_args()
 
