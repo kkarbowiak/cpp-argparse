0d184f83274dab98b477f88ed3adeb2fb0089702
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-01-24 23:12:59 +0100

    Add description to example application

--- examples/example_01.cpp
+++ examples/example_01.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char * argv[])
 {
-    auto parser = argparse::ArgumentParser();
+    auto parser = argparse::ArgumentParser().description("Example application illustrating simple use cases");
     parser.add_argument("pos1").help("positional argument #1");
     parser.add_argument("pos2").help("positional argument #2");
     parser.add_argument("pos3").help("positional argument #3");
