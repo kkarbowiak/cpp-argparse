a7ccd1c70c32c98eb64d1f8c71a44dd79ddcee87
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-02-17 18:51:31 +0100

    Extend unit test for nargs option

--- test/unittest/test_ArgumentParser_Parsing.cpp
+++ test/unittest/test_ArgumentParser_Parsing.cpp
@@ -718,5 +718,14 @@ TEST_CASE("Parsing an optional argument with nargs set...")
 
             CHECK(parsed.get_value<std::vector<std::string>>("o") == std::vector<std::string>{"foo"});
         }
+
+        SUBCASE("...for two arguments")
+        {
+            parser.add_argument("-o").nargs(2);
+
+            auto const parsed = parser.parse_args(4, cstr_arr{"prog", "-o", "foo", "bar"});
+
+            CHECK(parsed.get_value<std::vector<std::string>>("o") == std::vector<std::string>{"foo", "bar"});
+        }
     }
 }
