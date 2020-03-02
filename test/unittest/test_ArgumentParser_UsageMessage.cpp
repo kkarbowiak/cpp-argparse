d83e3b0ac175d298a7f36ed9d208ad5a178f654c
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-03 00:01:57 +0100

    Extend unit test for generating usage message

--- test/unittest/test_ArgumentParser_UsageMessage.cpp
+++ test/unittest/test_ArgumentParser_UsageMessage.cpp
@@ -439,5 +439,12 @@ TEST_CASE("Usage message contains...")
 
             CHECK(parser.format_usage() == "usage: prog [-o O [O ...]]"s);
         }
+
+        SUBCASE("...its name followed by metavar and metavar and ellipsis in brackets for arguments with metavar set and nargs set as +")
+        {
+            parser.add_argument("-o").metavar("METAVARO").nargs('+');
+
+            CHECK(parser.format_usage() == "usage: prog [-o METAVARO [METAVARO ...]]"s);
+        }
     }
 }
