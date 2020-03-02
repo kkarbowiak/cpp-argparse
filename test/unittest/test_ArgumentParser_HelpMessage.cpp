3960b3e2373449855ed01ba480194840ed31190f
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-03 00:42:28 +0100

    Extend unit test for generating help message

--- test/unittest/test_ArgumentParser_HelpMessage.cpp
+++ test/unittest/test_ArgumentParser_HelpMessage.cpp
@@ -209,6 +209,13 @@ TEST_CASE("Help message contains...")
                 CHECK(parser.format_help() == "usage: prog {\"foo\",\"bar\"} {\"foo\",\"bar\"} {\"foo\",\"bar\"}\n\npositional arguments:\n  {\"foo\",\"bar\"}"s);
             }
         }
+
+        SUBCASE("...name for argument with nargs set as ?")
+        {
+            parser.add_argument("p1").nargs('?');
+
+            CHECK(parser.format_help() == "usage: prog [p1]\n\npositional arguments:\n  p1"s);
+        }
     }
 
     SUBCASE("...for optional argument...")
