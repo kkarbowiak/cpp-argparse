88265fdb3d3dfa2615f66ef0dfd5a0a681d51c43
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-02 23:56:06 +0100

    Extend optional argument nargs support

--- test/unittest/test_ArgumentParser_UsageMessage.cpp
+++ test/unittest/test_ArgumentParser_UsageMessage.cpp
@@ -425,5 +425,12 @@ TEST_CASE("Usage message contains...")
 
             CHECK(parser.format_usage() == "usage: prog [-o [METAVARO [METAVARO ...]]]"s);
         }
+
+        SUBCASE("...its name followed by choices in brackets and choices and ellipsis in nested brackets for arguments with choices set and nargs set as *")
+        {
+            parser.add_argument("-o").choices({"foo"s, "bar"s}).nargs('*');
+
+            CHECK(parser.format_usage() == "usage: prog [-o [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]]"s);
+        }
     }
 }
