1e89b9c18bc1ad660a39cc4b6fecc97d10eef864
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-03 00:33:50 +0100

    Extend unit test for generating help message

--- test/unittest/test_ArgumentParser_HelpMessage.cpp
+++ test/unittest/test_ArgumentParser_HelpMessage.cpp
@@ -161,6 +161,13 @@ TEST_CASE("Help message contains...")
                 CHECK(parser.format_help() == "usage: prog p1 p1 p1\n\npositional arguments:\n  p1"s);
             }
         }
+
+        SUBCASE("...metavar for argument with metavar set and nargs set as number")
+        {
+            parser.add_argument("p1").metavar("metap1").nargs(1);
+
+            CHECK(parser.format_help() == "usage: prog metap1\n\npositional arguments:\n  metap1"s);
+        }
     }
 
     SUBCASE("...for optional argument...")
