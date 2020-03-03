ec1d230f0a175f9abc5ced91c86e6116c8f1a090
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-03 17:29:08 +0100

    Extend unit test for generating help message

--- test/unittest/test_ArgumentParser_HelpMessage.cpp
+++ test/unittest/test_ArgumentParser_HelpMessage.cpp
@@ -444,6 +444,13 @@ TEST_CASE("Help message contains...")
 
                 CHECK(parser.format_help() == "usage: prog [-o {\"foo\",\"bar\"} {\"foo\",\"bar\"}]\n\noptional arguments:\n  -o {\"foo\",\"bar\"} {\"foo\",\"bar\"}"s);
             }
+
+            SUBCASE("...3")
+            {
+                parser.add_argument("-o").choices({"foo"s, "bar"s}).nargs(3);
+
+                CHECK(parser.format_help() == "usage: prog [-o {\"foo\",\"bar\"} {\"foo\",\"bar\"} {\"foo\",\"bar\"}]\n\noptional arguments:\n  -o {\"foo\",\"bar\"} {\"foo\",\"bar\"} {\"foo\",\"bar\"}"s);
+            }
         }
     }
 }
