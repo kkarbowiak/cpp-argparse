5873e043752cc5d740371b8016c6e6fbad4554be
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-05 17:26:09 +0100

    Extend unit test for parsing optional argument

--- test/unittest/test_ArgumentParser_Parsing.cpp
+++ test/unittest/test_ArgumentParser_Parsing.cpp
@@ -921,7 +921,7 @@ TEST_CASE("An optional argument does not consume another optional argument...")
         parser.add_argument("-o");
         parser.add_argument("-p");
 
-        CHECK_THROWS(parser.parse_args(3, cstr_arr{"prog", "-o", "-p"}));
+        CHECK_THROWS_WITH_AS(parser.parse_args(3, cstr_arr{"prog", "-o", "-p"}), "argument -o: expected one argument", argparse::parsing_error);
     }
 
     SUBCASE("...for argument with nargs set as number...")
