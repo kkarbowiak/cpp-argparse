cb5bda96ebed00f9fdd31bc599406ad15d4d8908
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-20 22:53:48 +0100

    Extend unit test for parsing positional arguments with choices and nargs

--- test/unittest/test_ArgumentParser_Parsing.cpp
+++ test/unittest/test_ArgumentParser_Parsing.cpp
@@ -1040,6 +1040,15 @@ TEST_CASE("Parsing a positional argument with choices set...")
 
                 CHECK(parsed.get_value<std::vector<std::string>>("pos") == std::vector<std::string>{"foo", "bar"});
             }
+
+            SUBCASE("...for three arguments")
+            {
+                parser.add_argument("pos").choices({"foo"s, "bar"s}).nargs(3);
+
+                auto const parsed = parser.parse_args(4, cstr_arr{"prog", "foo", "bar", "foo"});
+
+                CHECK(parsed.get_value<std::vector<std::string>>("pos") == std::vector<std::string>{"foo", "bar", "foo"});
+            }
         }
     }
 }
