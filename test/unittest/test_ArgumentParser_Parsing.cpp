37cde881c16e854f5e4c93e5e1608acca1150d1a
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-03-25 00:12:12 +0100

    Extend unit test for parsing optional arguments with choices and nargs

--- test/unittest/test_ArgumentParser_Parsing.cpp
+++ test/unittest/test_ArgumentParser_Parsing.cpp
@@ -1445,6 +1445,13 @@ TEST_CASE("Parsing an optional argument with choices set...")
 
                 CHECK_THROWS_WITH_AS(parser.parse_args(4, cstr_arr{"prog", "-o", "foo", "baz"}), "argument -o: invalid choice: \"baz\" (choose from \"foo\", \"bar\")", argparse::parsing_error);
             }
+
+            SUBCASE("...for three arguments")
+            {
+                parser.add_argument("-o").choices({"foo"s, "bar"s}).nargs('+');
+
+                CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-o", "foo", "bar", "baz"}), "argument -o: invalid choice: \"baz\" (choose from \"foo\", \"bar\")", argparse::parsing_error);
+            }
         }
     }
 }
