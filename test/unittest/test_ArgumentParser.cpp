372dc0a2647b16fee666269b18dcef2403cfa815
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-04-10 00:41:17 +0200

    Extend unit test for argument parser usage parameter

--- test/unittest/test_ArgumentParser.cpp
+++ test/unittest/test_ArgumentParser.cpp
@@ -121,11 +121,19 @@ TEST_CASE("ArgumentParser uses prog parameter as its name...")
     }
 }
 
-TEST_CASE("ArgumentParser uses usage parameter as its usage")
+TEST_CASE("ArgumentParser uses usage parameter as its usage...")
 {
-    auto parser = argparse::ArgumentParser().prog("prog").usage("program [options]");
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false).usage("program [options]");
 
-    CHECK(parser.format_usage() == "usage: program [options]"s);
+    SUBCASE("...in usage message")
+    {
+        CHECK(parser.format_usage() == "usage: program [options]"s);
+    }
+
+    SUBCASE("...in help message")
+    {
+        CHECK(parser.format_help() == "usage: program [options]"s);
+    }
 }
 
 TEST_CASE("Adding a positional argument with required option set results in error")
