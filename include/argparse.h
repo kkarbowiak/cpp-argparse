1f6a977d1cf987b2af7ccbd59fe80778f3be5362
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-04-15 21:51:30 +0200

    Fix extracting prog name in case of using backslash separators

--- include/argparse.h
+++ include/argparse.h
@@ -266,7 +266,7 @@ namespace argparse
         private:
             auto extract_filename(std::string path) -> std::string
             {
-                if (auto path_separator = path.rfind("/"); path_separator != std::string::npos)
+                if (auto path_separator = path.find_last_of("/\\"); path_separator != std::string::npos)
                 {
                     return path.substr(path_separator + 1);
                 }
