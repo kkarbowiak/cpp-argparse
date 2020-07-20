82032a64602d286d6a78dd251fff65ff672aefc1
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-07-21 00:26:42 +0200

    Extend unit test for error message contents

--- test/unittest/test_error_message.cpp
+++ test/unittest/test_error_message.cpp
@@ -9,24 +9,38 @@ TEST_CASE("Missing required arguments message lists all argument's names...")
 {
     auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
 
-    SUBCASE("...for argument with one name")
+    SUBCASE("...for one argument...")
     {
-        parser.add_argument("-o").required(true);
+        SUBCASE("...with one name")
+        {
+            parser.add_argument("-o").required(true);
 
-        CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o", argparse::parsing_error);
+            CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o", argparse::parsing_error);
+        }
+
+        SUBCASE("...with two names")
+        {
+            parser.add_argument("-o", "--option").required(true);
+
+            CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o/--option", argparse::parsing_error);
+        }
+
+        SUBCASE("...with three names")
+        {
+            parser.add_argument("-o", "--option", "--long-option").required(true);
+
+            CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o/--option/--long-option", argparse::parsing_error);
+        }
     }
 
-    SUBCASE("...for argument with two names")
+    SUBCASE("...for two arguments...")
     {
-        parser.add_argument("-o", "--option").required(true);
+        SUBCASE("...with one name")
+        {
+            parser.add_argument("-o").required(true);
+            parser.add_argument("-r").required(true);
 
-        CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o/--option", argparse::parsing_error);
-    }
-
-    SUBCASE("...for argument with three names")
-    {
-        parser.add_argument("-o", "--option", "--long-option").required(true);
-
-        CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o/--option/--long-option", argparse::parsing_error);
+            CHECK_THROWS_WITH_AS(parser.parse_args(1, cstr_arr{"prog"}), "the following arguments are required: -o -r", argparse::parsing_error);
+        }
     }
 }
