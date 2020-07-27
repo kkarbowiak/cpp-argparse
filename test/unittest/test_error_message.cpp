95df5d01c634cf19c505d9c129b4add4475ce944
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2020-07-28 01:05:04 +0200

    Fix error message for optional arguments with multiple names

--- test/unittest/test_error_message.cpp
+++ test/unittest/test_error_message.cpp
@@ -167,12 +167,24 @@ TEST_CASE("Expected one argument message lists all optional argument's names..."
     }
 }
 
-TEST_CASE("Argument not allowed with argument message lists all optional argument's names")
+TEST_CASE("Argument not allowed with argument message lists all optional argument's names...")
 {
     auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
     auto group = parser.add_mutually_exclusive_group();
-    group.add_argument("-a");
-    group.add_argument("-b");
 
-    CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-b", "b"}), "argument -b: not allowed with argument -a", argparse::parsing_error);
+    SUBCASE("...for arguments with one name")
+    {
+        group.add_argument("-a");
+        group.add_argument("-b");
+
+        CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-b", "b"}), "argument -b: not allowed with argument -a", argparse::parsing_error);
+    }
+
+    SUBCASE("...for arguments with two names")
+    {
+        group.add_argument("-a", "--aaa");
+        group.add_argument("-b", "--bbb");
+
+        CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-b", "b"}), "argument -b/--bbb: not allowed with argument -a/--aaa", argparse::parsing_error);
+    }
 }
