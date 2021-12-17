97e78d785ebeceedd4f55dd442a6181bf259873e
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2021-12-17 21:53:23 +0100

    Change content of example program

--- examples/example_01.cpp
+++ examples/example_01.cpp
@@ -4,47 +4,31 @@
 #include <iostream>
 
 
+using namespace std::string_literals;
+
 int main(int argc, char * argv[])
 {
-    auto parser = argparse::ArgumentParser().description("Example application illustrating simple use cases").epilog("And handling of multiple arguments");
-    parser.add_argument("pos1").help("positional argument #1");
-    parser.add_argument("pos2").help("positional argument #2");
-    parser.add_argument("pos3").help("positional argument #3");
-    parser.add_argument("-f").help("optional argument #1");
-    parser.add_argument("-g").help("optional argument #2").action(argparse::store_true);
-    parser.add_argument("--long-option").help("optional argument #4");
-    parser.add_argument("--very-long-option").help("optional argument #5").action(argparse::store_true);
+    auto parser = argparse::ArgumentParser();
+    parser.add_argument("arg1").help("argument");
+    parser.add_argument("arg2").help("argument of int type").type<int>();
+    parser.add_argument("arg3").help("argument that only accepts 'foo' or 'bar' values").choices({"foo"s, "bar"s});
 
     try
     {
         auto args = parser.parse_args(argc, argv);
 
-        std::cout << std::boolalpha << "args:\n";
-        for (std::string a : {"pos1", "pos2", "pos3", "f", "g", "help", "long_option", "very_long_option"})
-        {
-            std::cout << a << ": ";
-            if (auto v = args.get(a))
-            {
-                if (a == "g" || a == "help" || a == "very_long_option")
-                {
-                    std::cout << v.get<bool>();
-                }
-                else
-                {
-                    std::cout << v.get();
-                }
-            }
-            else
-            {
-                std::cout << "<?>";
-            }
-            std::cout << '\n';
-        }
+        auto const arg1_value = args.get_value("arg1");
+        auto const arg2_value = args.get_value<int>("arg2");
+        auto const arg3_value = args.get_value("arg3");
+
+        std::cout << "args:\n";
+        std::cout << "  arg1: " << arg1_value << '\n';
+        std::cout << "  arg2: " << arg2_value << '\n';
+        std::cout << "  arg3: " << arg3_value << '\n';
     }
     catch (std::exception const & e)
     {
         std::cout << e.what() << '\n';
-        return 0;
     }
 
     return 0;
