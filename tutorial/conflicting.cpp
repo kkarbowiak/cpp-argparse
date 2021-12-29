ac7d3c9ff75a2b0d6f2a689a93875f44bd8c6d17
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2021-12-29 22:15:59 +0100

    Expand the tutorial and add examples

---
+++ tutorial/conflicting.cpp
@@ -0,0 +1,29 @@
+#include "argparse.h"
+#include <iostream>
+#include <cmath>
+
+int main(int argc, char * argv[])
+{
+    auto parser = argparse::ArgumentParser();
+    auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-v", "--verbose").action(argparse::store_true);
+    group.add_argument("-q", "--quiet").action(argparse::store_true);
+    parser.add_argument("x").type<int>().help("the base");
+    parser.add_argument("y").type<int>().help("the exponent");
+    auto parsed = parser.parse_args(argc, argv);
+    auto base = parsed.get_value<int>("x");
+    auto exp = parsed.get_value<int>("y");
+    auto answer = std::pow(base, exp);
+    if (parsed.get_value<bool>("quiet"))
+    {
+        std::cout << answer << '\n';
+    }
+    else if (parsed.get_value<bool>("verbose"))
+    {
+        std::cout << base << " to the power " << exp << " equals " << answer << '\n';
+    }
+    else
+    {
+        std::cout << base << "^" << exp << " == " << answer << '\n';
+    }
+}
