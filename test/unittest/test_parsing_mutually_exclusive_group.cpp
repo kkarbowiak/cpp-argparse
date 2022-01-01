bc8ec0f06e82a1881a910db12a305999d489e7cb
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-01-01 21:44:37 +0100

    Refactor: break subsections into separate test cases

--- test/unittest/test_parsing_mutually_exclusive_group.cpp
+++ test/unittest/test_parsing_mutually_exclusive_group.cpp
@@ -5,38 +5,45 @@
 #include "doctest.h"
 
 
-TEST_CASE("Parsing mutually exclusive group...")
+TEST_CASE("Parsing mutually exclusive group of arguments with store action does not throw for no arguments")
 {
     auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
     auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-a");
+    group.add_argument("-b");
 
-    SUBCASE("...of arguments with store action...")
-    {
-        group.add_argument("-a");
-        group.add_argument("-b");
+    CHECK_NOTHROW(parser.parse_args(1, cstr_arr{"prog"}));
+}
 
-        SUBCASE("...does not throw for no arguments")
-        {
-            CHECK_NOTHROW(parser.parse_args(1, cstr_arr{"prog"}));
-        }
+TEST_CASE("Parsing mutually exclusive group of arguments with store action does not throw for a single argument")
+{
+    auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
+    auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-a");
+    group.add_argument("-b");
 
-        SUBCASE("...does not throw for a single argument")
-        {
-            CHECK_NOTHROW(parser.parse_args(3, cstr_arr{"prog", "-a", "a"}));
-            CHECK_NOTHROW(parser.parse_args(3, cstr_arr{"prog", "-b", "b"}));
-        }
+    CHECK_NOTHROW(parser.parse_args(3, cstr_arr{"prog", "-a", "a"}));
+    CHECK_NOTHROW(parser.parse_args(3, cstr_arr{"prog", "-b", "b"}));
+}
 
-        SUBCASE("..does not throw for arguments in separate groups")
-        {
-            auto other_group = parser.add_mutually_exclusive_group();
-            other_group.add_argument("-c");
+TEST_CASE("Parsing mutually exclusive group of arguments with store action does not throw for arguments in separate groups")
+{
+    auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
+    auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-a");
+    group.add_argument("-b");
+    auto other_group = parser.add_mutually_exclusive_group();
+    other_group.add_argument("-c");
 
-            CHECK_NOTHROW(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-c", "c"}));
-        }
+    CHECK_NOTHROW(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-c", "c"}));
+}
 
-        SUBCASE("...throws for exclusive arguments")
-        {
-            CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-b", "b"}), "argument -b: not allowed with argument -a", argparse::parsing_error);
-        }
-    }
+TEST_CASE("Parsing mutually exclusive group of arguments with store action throws for exclusive arguments")
+{
+    auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
+    auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-a");
+    group.add_argument("-b");
+
+    CHECK_THROWS_WITH_AS(parser.parse_args(5, cstr_arr{"prog", "-a", "a", "-b", "b"}), "argument -b: not allowed with argument -a", argparse::parsing_error);
 }
