0319855f64a67acb7a5dc702d490a1a61050ca85
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-08-27 00:20:55 +0200

    Extend unit test for parsing '--' pseudo argument

--- test/unittest/test_parsing.cpp
+++ test/unittest/test_parsing.cpp
@@ -3006,10 +3006,21 @@ TEST_CASE("Parsing -- pseudo argument does not throw")
     CHECK_NOTHROW(parser.parse_args(2, cstr_arr{"prog", "--"}));
 }
 
-TEST_CASE("An optional argument does not consume arguments past the -- pseudo argument")
+TEST_CASE("An optional argument does not consume arguments past the -- pseudo argument...")
 {
     auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
-    parser.add_argument("-o");
 
-    CHECK_THROWS_WITH_AS(parser.parse_args(3, cstr_arr{"prog", "-o", "--"}), "argument -o: expected one argument", argparse::parsing_error);
+    SUBCASE("...for simple argument")
+    {
+        parser.add_argument("-o");
+
+        CHECK_THROWS_WITH_AS(parser.parse_args(3, cstr_arr{"prog", "-o", "--"}), "argument -o: expected one argument", argparse::parsing_error);
+    }
+
+    SUBCASE("...for argument with nargs set as number")
+    {
+        parser.add_argument("-o").nargs(1);
+
+        CHECK_THROWS_WITH_AS(parser.parse_args(3, cstr_arr{"prog", "-o", "--"}), "argument -o: expected 1 argument", argparse::parsing_error);
+    }
 }
