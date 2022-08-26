5d3eb53ae278581cdd6da972651d0caf5cf15386
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-08-27 01:30:23 +0200

    Cleanup: remove no longer needed condition

--- include/argparse.h
+++ include/argparse.h
@@ -355,7 +355,7 @@ namespace argparse
 
             auto ensure_no_unrecognised_arguments(tokens const & args) const -> void
             {
-                if (!args.empty() && !(args.size() == 1 && args.front() == "--"))
+                if (!args.empty())
                 {
                     throw parsing_error("unrecognised arguments: " + join(args, " "));
                 }
