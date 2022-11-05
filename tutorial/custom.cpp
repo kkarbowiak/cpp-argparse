2ffc6bcedeb2f3d7dec10ba106adeaf0ded92275
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-11-05 12:22:19 +0100

    Use default member initialisers

--- tutorial/custom.cpp
+++ tutorial/custom.cpp
@@ -7,16 +7,14 @@ namespace geometry
 {
     struct Point
     {
-        Point() : x(0), y(0)
-        {
-        }
+        Point() = default;
 
         Point(int x, int y) : x(x), y(y)
         {
         }
 
-        int x;
-        int y;
+        int x = 0;
+        int y = 0;
     };
 }
 
