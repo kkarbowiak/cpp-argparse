782e00191f51b2bbf5cadb2de67fe5a7e4ddfd03
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-01-29 23:20:57 +0100

    Use trailing function return type

--- test/manual/main.cpp
+++ test/manual/main.cpp
@@ -1,7 +1,7 @@
 #include "argparse.h"
 
 
-int main(int argc, char * argv[])
+auto main(int argc, char * argv[]) -> int
 {
     auto parser = argparse::ArgumentParser();
     parser.add_argument("positional");
