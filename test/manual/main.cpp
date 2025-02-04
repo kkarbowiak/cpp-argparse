9f3cd3d511848d7829e6e5705f1bd281ba02fa82
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-02-04 21:03:30 +0100

    Extend simple application

--- test/manual/main.cpp
+++ test/manual/main.cpp
@@ -1,4 +1,5 @@
 #include "argparse.h"
+#include <iostream>
 
 
 auto main(int argc, char * argv[]) -> int
@@ -7,5 +8,7 @@ auto main(int argc, char * argv[]) -> int
     parser.add_argument("positional");
     parser.add_argument("--optional");
     parser.add_argument("-v", "--version").action(argparse::version).version("1.0.0");
-    parser.parse_args(argc, argv);
+    auto const args = parser.parse_args(argc, argv);
+    std::cout << "positional: " << args.get_value("positional") << '\n';
+    std::cout << "optional: " << (args.get("optional") ? args.get_value("optional") : "<none>") << '\n';
 }
