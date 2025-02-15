3c344d447a12770a7ed55199dc893e5c5c7933a7
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-02-15 12:35:54 +0100

    Flatten test case

--- test/unittest/test_parsing_mutually_exclusive_group.cpp
+++ test/unittest/test_parsing_mutually_exclusive_group.cpp
@@ -120,14 +120,17 @@ TEST_CASE("Parsing mutually exclusive group of arguments with store false action
     group.add_argument("-a").action(argparse::store_false);
     group.add_argument("-b").action(argparse::store_false);
 
-    SUBCASE("")
-    {
-        CHECK_NOTHROW(parser.parse_args(2, cstr_arr{"prog", "-a"}));
-    }
-    SUBCASE("")
-    {
-        CHECK_NOTHROW(parser.parse_args(2, cstr_arr{"prog", "-b"}));
-    }
+    CHECK_NOTHROW(parser.parse_args(2, cstr_arr{"prog", "-a"}));
+}
+
+TEST_CASE("Parsing mutually exclusive group of arguments with store false action does not throw for a single argument")
+{
+    auto parser = argparse::ArgumentParser().handle(argparse::Handle::none);
+    auto group = parser.add_mutually_exclusive_group();
+    group.add_argument("-a").action(argparse::store_false);
+    group.add_argument("-b").action(argparse::store_false);
+
+    CHECK_NOTHROW(parser.parse_args(2, cstr_arr{"prog", "-b"}));
 }
 
 TEST_CASE("Parsing mutually exclusive group of arguments with store false action does not throw for arguments in separate groups")
