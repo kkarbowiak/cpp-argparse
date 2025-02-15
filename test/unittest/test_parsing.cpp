7f37fe9186663d2754e0ddee76d7f2f2d89371e4
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-02-15 13:02:19 +0100

    Flatten test case

--- test/unittest/test_parsing.cpp
+++ test/unittest/test_parsing.cpp
@@ -55,24 +55,22 @@ TEST_CASE("Parsing an optional argument...")
     }
 }
 
-TEST_CASE("Parsing an optional argument with store true action...")
+TEST_CASE("Parsing an optional argument with store true action yields false when it's missing")
 {
     auto parser = argparse::ArgumentParser();
     parser.add_argument("-o").action(argparse::store_true);
+    auto const parsed = parser.parse_args(1, cstr_arr{"prog"});
 
-    SUBCASE("...yields false when it's missing")
-    {
-        auto const parsed = parser.parse_args(1, cstr_arr{"prog"});
+    CHECK(parsed.get_value<bool>("o") == false);
+}
 
-        CHECK(parsed.get_value<bool>("o") == false);
-    }
+TEST_CASE("Parsing an optional argument with store true action yields true when it's present")
+{
+    auto parser = argparse::ArgumentParser();
+    parser.add_argument("-o").action(argparse::store_true);
+    auto const parsed = parser.parse_args(2, cstr_arr{"prog", "-o"});
 
-    SUBCASE("...yields true when it's present")
-    {
-        auto const parsed = parser.parse_args(2, cstr_arr{"prog", "-o"});
-
-        CHECK(parsed.get_value<bool>("o") == true);
-    }
+    CHECK(parsed.get_value<bool>("o") == true);
 }
 
 TEST_CASE("Parsing an optional argument with store false action yields true when it's missing")
