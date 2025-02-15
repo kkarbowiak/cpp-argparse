64f83d7a7ff72faf3002ebd4d9ddc440ea995ea5
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-02-15 11:46:52 +0100

    Flatten test case

--- test/unittest/test_help_message.cpp
+++ test/unittest/test_help_message.cpp
@@ -12,96 +12,100 @@
 
 using namespace std::string_literals;
 
-TEST_CASE("ArgumentParser provides help message...")
+TEST_CASE("ArgumentParser provides help message with usage section")
 {
     auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    CHECK(parser.format_help() == "usage: prog"s);
+}
 
-    SUBCASE("...with usage section")
-    {
-        CHECK(parser.format_help() == "usage: prog"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and description for description and no arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").description("A foo that bars").add_help(false);
 
-    SUBCASE("...with usage section and description for description and no arguments")
-    {
-        auto parser = argparse::ArgumentParser().prog("prog").description("A foo that bars").add_help(false);
+    CHECK(parser.format_help() == "usage: prog\n\nA foo that bars"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog\n\nA foo that bars"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and epilog for epilog and no arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").epilog("And that's how you'd foo a bar").add_help(false);
 
-    SUBCASE("...with usage section and epilog for epilog and no arguments")
-    {
-        auto parser = argparse::ArgumentParser().prog("prog").epilog("And that's how you'd foo a bar").add_help(false);
+    CHECK(parser.format_help() == "usage: prog\n\nAnd that's how you'd foo a bar"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog\n\nAnd that's how you'd foo a bar"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and positional arguments section for one positional argument")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("p1");
 
-    SUBCASE("...with usage section and positional arguments section for one positional argument")
-    {
-        parser.add_argument("p1");
+    CHECK(parser.format_help() == "usage: prog p1\n\npositional arguments:\n  p1"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog p1\n\npositional arguments:\n  p1"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and positional arguments section for two positional arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("p1");
+    parser.add_argument("p2");
 
-    SUBCASE("...with usage section and positional arguments section for two positional arguments")
-    {
-        parser.add_argument("p1");
-        parser.add_argument("p2");
+    CHECK(parser.format_help() == "usage: prog p1 p2\n\npositional arguments:\n  p1\n  p2"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog p1 p2\n\npositional arguments:\n  p1\n  p2"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and positional arguments section for three positional arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("p1");
+    parser.add_argument("p2");
+    parser.add_argument("p3");
 
-    SUBCASE("...with usage section and positional arguments section for three positional arguments")
-    {
-        parser.add_argument("p1");
-        parser.add_argument("p2");
-        parser.add_argument("p3");
+    CHECK(parser.format_help() == "usage: prog p1 p2 p3\n\npositional arguments:\n  p1\n  p2\n  p3"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog p1 p2 p3\n\npositional arguments:\n  p1\n  p2\n  p3"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and optional arguments section for one optional argument")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("-o");
 
-    SUBCASE("...with usage section and optional arguments section for one optional argument")
-    {
-        parser.add_argument("-o");
+    CHECK(parser.format_help() == "usage: prog [-o O]\n\noptional arguments:\n  -o O"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog [-o O]\n\noptional arguments:\n  -o O"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and optional arguments section for two optional arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("-o");
+    parser.add_argument("--option");
 
-    SUBCASE("...with usage section and optional arguments section for two optional arguments")
-    {
-        parser.add_argument("-o");
-        parser.add_argument("--option");
+    CHECK(parser.format_help() == "usage: prog [-o O] [--option OPTION]\n\noptional arguments:\n  -o O\n  --option OPTION"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog [-o O] [--option OPTION]\n\noptional arguments:\n  -o O\n  --option OPTION"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section and optional arguments section for three optional arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("-o");
+    parser.add_argument("--option");
+    parser.add_argument("--very-long-name");
 
-    SUBCASE("...with usage section and optional arguments section for three optional arguments")
-    {
-        parser.add_argument("-o");
-        parser.add_argument("--option");
-        parser.add_argument("--very-long-name");
+    CHECK(parser.format_help() == "usage: prog [-o O] [--option OPTION] [--very-long-name VERY_LONG_NAME]\n\noptional arguments:\n  -o O\n  --option OPTION\n  --very-long-name VERY_LONG_NAME"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog [-o O] [--option OPTION] [--very-long-name VERY_LONG_NAME]\n\noptional arguments:\n  -o O\n  --option OPTION\n  --very-long-name VERY_LONG_NAME"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section, positional arguments section, and optional arguments section for one positional and one optional argument")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("p1");
+    parser.add_argument("-o");
 
-    SUBCASE("...with usage section, positional arguments section, and optional arguments section for one positional and one optional argument")
-    {
-        parser.add_argument("p1");
-        parser.add_argument("-o");
+    CHECK(parser.format_help() == "usage: prog [-o O] p1\n\npositional arguments:\n  p1\n\noptional arguments:\n  -o O"s);
+}
 
-        CHECK(parser.format_help() == "usage: prog [-o O] p1\n\npositional arguments:\n  p1\n\noptional arguments:\n  -o O"s);
-    }
+TEST_CASE("ArgumentParser provides help message with usage section, positional arguments section, and optional arguments section for three positional and three optional arguments")
+{
+    auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
+    parser.add_argument("p1");
+    parser.add_argument("-o");
+    parser.add_argument("p2");
+    parser.add_argument("-a");
+    parser.add_argument("p3");
+    parser.add_argument("-z");
 
-    SUBCASE("...with usage section, positional arguments section, and optional arguments section for three positional and three optional arguments")
-    {
-        parser.add_argument("p1");
-        parser.add_argument("-o");
-        parser.add_argument("p2");
-        parser.add_argument("-a");
-        parser.add_argument("p3");
-        parser.add_argument("-z");
-
-        CHECK(parser.format_help() == "usage: prog [-o O] [-a A] [-z Z] p1 p2 p3\n\npositional arguments:\n  p1\n  p2\n  p3\n\noptional arguments:\n  -o O\n  -a A\n  -z Z"s);
-    }
+    CHECK(parser.format_help() == "usage: prog [-o O] [-a A] [-z Z] p1 p2 p3\n\npositional arguments:\n  p1\n  p2\n  p3\n\noptional arguments:\n  -o O\n  -a A\n  -z Z"s);
 }
 
 TEST_CASE("Help message contains for positional argument name")
