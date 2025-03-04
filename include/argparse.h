fabef0134cce2351e7b68bf7929fd1b875e93ec1
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-04 23:32:58 +0100

    Refactor: use view pipe syntax

--- include/argparse.h
+++ include/argparse.h
@@ -687,7 +687,7 @@ namespace argparse
 
                     auto parse_args(tokens args) -> tokens override
                     {
-                        auto consumable = std::ranges::drop_while_view(args, [](auto const & token) { return token.m_consumed; });
+                        auto consumable = args | std::views::drop_while([](auto const & token) { return token.m_consumed; });
                         if (has_nargs())
                         {
                             if (has_nargs_number())
