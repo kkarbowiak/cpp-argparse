3136f546a46dfdcea7f201ca40672f200e9f86f9
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-17 21:36:58 +0100

    Extend unit test

--- test/unittest/test_parsing_optional.cpp
+++ test/unittest/test_parsing_optional.cpp
@@ -143,10 +143,14 @@ TEST_CASE_TEMPLATE("Parsing an optional argument with store const action yields 
     }
 }
 
-TEST_CASE("Parser accepts count action")
+TEST_CASE("Parsing an optional argument with count action yields false when it's missing")
 {
     auto parser = argparse::ArgumentParser();
     parser.add_argument("-c").action(argparse::count);
+
+    auto const parsed = parser.parse_args(1, cstr_arr{"prog"});
+
+    CHECK(!parsed.get("c"));
 }
 
 TEST_CASE("Parsing an optional argument with help action yields false when it's missing")
