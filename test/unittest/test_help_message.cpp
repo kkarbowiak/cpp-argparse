850db4ec803de8c0d0ca134a4ca70754aa88e21b
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-24 16:33:52 +0100

    Replace all occurrences

--- test/unittest/test_help_message.cpp
+++ test/unittest/test_help_message.cpp
@@ -672,6 +672,13 @@ TEST_CASE("ArgumentParser replaces '{prog}' with program name in description tex
     CHECK(parser.format_help() == "usage: program\n\nA program that bars"s);
 }
 
+TEST_CASE("ArgumentParser replaces '{prog}' with program name in description text")
+{
+    auto parser = argparse::ArgumentParser().prog("program").description("A {prog} that {prog}s").add_help(false);
+
+    CHECK(parser.format_help() == "usage: program\n\nA program that programs"s);
+}
+
 TEST_CASE("ArgumentParser replaces '{prog}' with program name in epilog text")
 {
     auto parser = argparse::ArgumentParser().prog("program").epilog("And that's how you'd foo a bar using {prog}").add_help(false);
