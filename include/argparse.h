ece86314427b2a71cea7eada77ba1741fee0254e
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-25 21:32:11 +0100

    Fix silently discarding joined value

--- include/argparse.h
+++ include/argparse.h
@@ -934,6 +934,11 @@ namespace argparse
                                         m_value = m_options.const_;
                                         break;
                                     case count:
+                                        if (!value.empty())
+                                        {
+                                            throw parsing_error(std::format("argument {}: ignored explicit argument '{}'", join(get_names(), "/"), value));
+                                        }
+
                                         if (!m_value.has_value())
                                         {
                                             m_value = 1;
