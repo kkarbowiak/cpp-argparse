170e4ff3ece45310b3bb3a20cb2fa7031a315f71
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-27 23:29:07 +0100

    Cleanup: improve text formatting in test

--- test/unittest/test_help_message.cpp
+++ test/unittest/test_help_message.cpp
@@ -353,7 +353,10 @@ TEST_CASE("Help message contains for positional argument choices for argument wi
     auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
     parser.add_argument("p1").choices({"foo"s, "bar"s}).nargs(argparse::zero_or_more);
 
-    CHECK(parser.format_help() == "usage: prog [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]\n\npositional arguments:\n  {\"foo\",\"bar\"}"s);
+    CHECK(parser.format_help() == "usage: prog [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]\n"
+                                  "\n"
+                                  "positional arguments:\n"
+                                  "  {\"foo\",\"bar\"}"s);
 }
 
 TEST_CASE("Help message contains for positional argument name for argument with nargs set as one_or_more")
