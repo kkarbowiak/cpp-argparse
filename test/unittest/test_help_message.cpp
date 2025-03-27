a2667f2a6f8f2efdfab7327090176b59e7a23ac5
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-27 23:29:49 +0100

    Cleanup: improve text formatting in test

--- test/unittest/test_help_message.cpp
+++ test/unittest/test_help_message.cpp
@@ -408,7 +408,10 @@ TEST_CASE("Help message contains for optional argument name and long name for ar
     auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
     parser.add_argument("-o", "--option").action(argparse::store_true);
 
-    CHECK(parser.format_help() == "usage: prog [-o]\n\noptional arguments:\n  -o, --option"s);
+    CHECK(parser.format_help() == "usage: prog [-o]\n"
+                                  "\n"
+                                  "optional arguments:\n"
+                                  "  -o, --option"s);
 }
 
 TEST_CASE("Help message contains for optional argument name and help for argument with store true action and help string")
