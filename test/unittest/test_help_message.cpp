54b59c20eb56454d6fb156a624a9da5cad005006
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-03-27 23:34:01 +0100

    Cleanup: improve text formatting in test

--- test/unittest/test_help_message.cpp
+++ test/unittest/test_help_message.cpp
@@ -870,7 +870,10 @@ TEST_CASE("Help message contains for optional argument name followed by choices 
     auto parser = argparse::ArgumentParser().prog("prog").add_help(false);
     parser.add_argument("-o").choices({"foo"s, "bar"s}).nargs(argparse::zero_or_more);
 
-    CHECK(parser.format_help() == "usage: prog [-o [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]]\n\noptional arguments:\n  -o [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]"s);
+    CHECK(parser.format_help() == "usage: prog [-o [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]]\n"
+                                  "\n"
+                                  "optional arguments:\n"
+                                  "  -o [{\"foo\",\"bar\"} [{\"foo\",\"bar\"} ...]]"s);
 }
 
 TEST_CASE("Help message contains for optional argument name followed by automatic metavar followed by automatic metavar and ellipsis in brackets for argument with nargs set as one_or_more")
