505154e49512924b070d2bba1effc132b89db35a
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-04-09 08:34:03 +0200

    Add unit test

--- test/unittest/test_argument_parser.cpp
+++ test/unittest/test_argument_parser.cpp
@@ -376,3 +376,12 @@ TEST_CASE("Requesting wrong argument type throws an exception")
 
     CHECK_THROWS_WITH(args.get_value<int>("pos"), doctest::Contains("wrong type"));
 }
+
+TEST_CASE("Requesting wrong argument type throws an exception")
+{
+    auto parser = argparse::ArgumentParser();
+    parser.add_argument("pos").type<int>();
+    auto args = parser.parse_args(2, cstr_arr{"prog", "3"});
+
+    CHECK_THROWS_WITH(args.get_value("pos"), doctest::Contains("wrong type"));
+}
