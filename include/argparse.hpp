4d5bd912a51370c409c508e050d622ad4dcba6ba
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-04-09 08:29:56 +0200

    Refactor: simplify code

--- include/argparse.hpp
+++ include/argparse.hpp
@@ -122,14 +122,12 @@ namespace argparse
                     template<typename T>
                     auto get() const -> T
                     {
-                        if (m_value.type() == typeid(T))
-                        {
-                            return std::any_cast<T>(m_value);
-                        }
-                        else
+                        if (m_value.type() != typeid(T))
                         {
                             throw std::runtime_error(std::format("wrong type: requested '{}' for argument of type '{}'", typeid(T).name(), m_value.type().name()));
                         }
+
+                        return std::any_cast<T>(m_value);
                     }
 
                 private:
