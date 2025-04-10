f8057bc11c0d0cdb345cefe18189b2d5d3a7f75c
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-04-10 16:48:10 +0200

    Add example file

---
+++ tutorial/count3.cpp
@@ -0,0 +1,32 @@
+#include "argparse.hpp"
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    try
+    {
+        auto parser = argparse::ArgumentParser();
+        parser.add_argument("square").help("display a square of a given number").type<int>();
+        parser.add_argument("-v", "--verbosity").help("increase output verbosity").action(argparse::count);
+        auto parsed = parser.parse_args(argc, argv);
+        auto value = parsed.get_value<int>("square");
+        auto answer = value * value;
+        auto verbosity = parsed.get("verbosity");
+        if (verbosity.get<int>() >= 2)
+        {
+            std::cout << "the square of " << value << " equals " << answer << '\n';
+        }
+        else if (verbosity.get<int>() >= 1)
+        {
+            std::cout << value << "^2 == " << answer << '\n';
+        }
+        else
+        {
+            std::cout << answer << '\n';
+        }
+    }
+    catch (argparse::type_error const & e)
+    {
+        std::cout << "error: " << e.what() << '\n';
+    }
+}
