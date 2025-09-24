a57624a44e1a32f5475f677d90f450dc43b2c4b2
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-09-24 22:22:48 +0200

    Extend script for checking tutorial sources

--- tutorial/check_tutorial.py
+++ tutorial/check_tutorial.py
@@ -1,9 +1,10 @@
 import glob
 import os.path
+import re
 
 
 def main():
-    cpp_files = sorted(glob.glob("*.cpp"))
+    cpp_files = glob.glob("*.cpp")
     check_presence_in_cmakelists(cpp_files)
     check_presence_in_readme(cpp_files)
     check_contents_in_readme(cpp_files)
@@ -29,11 +30,25 @@ def check_presence_in_readme(cpp_files):
 def check_contents_in_readme(cpp_files):
     with open("readme.md", "r") as f:
         readme_content = f.read()
+    sources = extract_code_blocks(readme_content)
+    sources_names = sorted([fname for fname in sources.keys()])
+    sym_diff = list(set(cpp_files) ^ set(sources_names))
+    if sym_diff:
+        print("The following files are mismatched between readme.md and the directory:")
+        for fname in sym_diff:
+            print(f"  {fname}")
     for cpp_file in cpp_files:
         with open(cpp_file, "r") as f:
-            cpp_content = f.read()
-        if cpp_content not in readme_content:
-            print(f"Warning: Contents of {cpp_file} are not included in readme.md")
+            code_in_file = f.read()
+        code_in_readme = sources.get(cpp_file, "")
+        if code_in_file != code_in_readme:
+            print(f"Warning: Content mismatch for {cpp_file} between file and readme.md")
+
+
+def extract_code_blocks(readme_content):
+    # Match: "(`filename.cpp``):\n```c++(...)```"
+    pattern = r"\(`([a-zA-Z0-9_]+\.cpp)`\):\s*```c\+\+\s*([\s\S]*?)```"
+    return dict(re.findall(pattern, readme_content))
 
 
 if __name__ == '__main__':
