91f34163278ffb5b7fdee4f28b34c4e0385d5cd6
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2025-10-01 20:23:06 +0200

    Add unit test

--- test/unittest/test_parsing_optional.cpp
+++ test/unittest/test_parsing_optional.cpp
@@ -2868,3 +2868,13 @@ TEST_CASE("Parsing an optional argument yields correct value for negative number
 
     CHECK(args.get_value<int>("n") == -65);
 }
+
+TEST_CASE("Parsing an optional argument yields correct value for negative number" * doctest::skip())
+{
+    auto parser = argparse::ArgumentParser();
+    parser.add_argument("--number").type<int>();
+
+    auto const args = parser.parse_args(3, cstr_arr{"prog", "--number", "-65"});
+
+    CHECK(args.get_value<int>("number") == -65);
+}
